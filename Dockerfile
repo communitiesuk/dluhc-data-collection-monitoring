f328bed4c55e62454993df9628e5e6d086387d01
Stéphane Meny <smeny@users.noreply.github.com>
2022-02-22 15:22:06 +0000

    Add script to read VCAP_SERVICES for Prometheus

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,4 @@
 FROM prom/prometheus
-ADD prometheus.yml /etc/prometheus/
+COPY prometheus_template.yml /etc/prometheus/prometheus_template.yml
+COPY scripts/setup_prometheus_config.sh /setup_prometheus_config.sh
+ENTRYPOINT ["./setup_prometheus_config.sh && /bin/prometheus"]
---
+++ scripts/setup_prometheus_config.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+INFLUXDB_CREDENTIALS=$(echo "${VCAP_SERVICES}" | jq '.influxdb[0].credentials.prometheus')
+
+INFLUXDB_READ_URL=$(echo "${INFLUXDB_CREDENTIALS}" | jq '.remote_read[0].url')
+INFLUXDB_READ_USERNAME=$(echo "${INFLUXDB_CREDENTIALS}" | jq '.remote_read[0].basic_auth.username')
+INFLUXDB_READ_PASSWORD=$(echo "${INFLUXDB_CREDENTIALS}" | jq '.remote_read[0].basic_auth.password')
+
+INFLUXDB_WRITE_URL=$(echo "${INFLUXDB_CREDENTIALS}" | jq '.remote_write[0].url')
+INFLUXDB_WRITE_USERNAME=$(echo "${INFLUXDB_CREDENTIALS}" | jq '.remote_write[0].basic_auth.username')
+INFLUXDB_WRITE_PASSWORD=$(echo "${INFLUXDB_CREDENTIALS}" | jq '.remote_write[0].basic_auth.password')
+
+envsubst < /etc/prometheus/prometheus_template.yml > /etc/prometheus/prometheus.yml
